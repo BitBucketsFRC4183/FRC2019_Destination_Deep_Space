8a3de81de1ccb5dfa4a40f476436bae5bdd42be6
mtkessel <the.rocketredneck@gmail.com>
2019-01-19 19:33:45 -0700

    Merged, tested servo control

--- DestinationDeepSpace/src/main/java/frc/robot/MotorId.java
+++ DestinationDeepSpace/src/main/java/frc/robot/MotorId.java
@@ -14,13 +14,20 @@ public class MotorId {
     
         /*DRIVE SUBSYSTEM*/
         public static final int LEFT_DRIVE_MOTOR_FRONT_ID  = 1;	// TODO: These are Junior's Numbers, consider 
+        public static final int LEFT_DRIVE_MOTOR_MIDDLE_ID = 0;
         public static final int LEFT_DRIVE_MOTOR_REAR_ID   = 4;
         public static final int RIGHT_DRIVE_MOTOR_FRONT_ID = 2;
+        public static final int RIGHT_DRIVE_MOTOR_MIDDLE_ID = 0;
         public static final int RIGHT_DRIVE_MOTOR_REAR_ID  = 3;
         
-        // TODO: actual IDs ... MOVE ALL MOTOR IDS to a separate MotorId class
         public static final int TOP_INTAKE_MOTOR_ID = 5;
         public static final int BOTTOM_INTAKE_MOTOR_ID = 6;
         public static final int ROTATION_MOTOR1_ID = 7;
         public static final int ROTATION_MOTOR2_ID = 8;
+
+        public static final int HIGH_CLIMB_MOTOR_1_ID = 9;
+        public static final int HIGH_CLIMB_MOTOR_2_ID = 10;
+
+
+    
 }
