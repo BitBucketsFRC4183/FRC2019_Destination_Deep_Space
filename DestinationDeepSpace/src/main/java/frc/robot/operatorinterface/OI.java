d78ace8bd038e274580a91e0ae327ce4ddd53ed2
Craig Post <cpost@bitbuckets.org>
2019-01-26 09:41:12 -0700

    Added brandButton to controllerMapper. Made controllerMapper default to ps4.

--- DestinationDeepSpace/src/main/java/frc/robot/operatorinterface/OI.java
+++ DestinationDeepSpace/src/main/java/frc/robot/operatorinterface/OI.java
@@ -16,7 +16,7 @@ public class OI {
 	private OI() {
 	}
 
-	private static JoystickConstants joystickConstants = JoystickConstants.xbox();
+	private static ControllerMapper controllerMapper = ControllerMapper.ps4();
 
 	private final static int DRIVER_JOYSTICK_ID = 0;
 	private final static int OPERATOR_JOYSTICK_ID = 1;
@@ -27,10 +27,10 @@ public class OI {
 	//****************************
 	// AXIS DEFINITIONS
 	//****************************
-    private final static int DRIVE_SPEED_AXIS            = joystickConstants.getLeftStickY();
-    private final static int DRIVE_TURN_AXIS             = joystickConstants.getRightStickX();
+    private final static int DRIVE_SPEED_AXIS            = controllerMapper.getLeftStickY();
+    private final static int DRIVE_TURN_AXIS             = controllerMapper.getRightStickX();
 
-	/** 
+	/**
 	 * speed - returns a speed command from driver joystick in the normal range [-1,1]
 	 * except when the invertDrive button is indicated, which causes the range to be [1,-1]
 	 */
@@ -79,15 +79,15 @@ public class OI {
 	//****************************
 	// BUTTON DEFINITIONS
 	//****************************
-	private final static int DRIVE_LOW_SENSITIVE_BUTTON  = joystickConstants.getR1();
-	private final static int DRIVE_INVERT_BUTTON         = joystickConstants.getR2();
-	private final static int DRIVE_ALIGN_LOCK_BUTTON     = joystickConstants.getL1();
-	private final static int DRIVE_LOCK_BUTTON     		 = joystickConstants.getL2();
+	private final static int DRIVE_LOW_SENSITIVE_BUTTON  = controllerMapper.getR1();
+	private final static int DRIVE_INVERT_BUTTON         = controllerMapper.getR2();
+	private final static int DRIVE_ALIGN_LOCK_BUTTON     = controllerMapper.getL1();
+	private final static int DRIVE_LOCK_BUTTON     		 = controllerMapper.getL2();
 
-	private final static int ARM_CLIMBER                 = PS4Constants.PS4.getValue();
-	private final static int CLIMB                       = PS4Constants.L_STICK.getValue();
+	private final static int ARM_CLIMBER                 = controllerMapper.getBrandButton();
+	private final static int CLIMB                       = controllerMapper.getLStickButton();
 
-	private final static int TEST_MOVE_BY_BUTTON         = PS4Constants.TRIANGLE.getValue(); /// TODO: Temp, use dashboard instead
+	private final static int TEST_MOVE_BY_BUTTON         = controllerMapper.getTriangle(); /// TODO: Temp, use dashboard instead
 
 	public boolean lowSensitivity()
 	{
