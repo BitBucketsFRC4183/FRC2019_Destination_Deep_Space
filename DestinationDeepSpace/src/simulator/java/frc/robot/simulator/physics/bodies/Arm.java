539ceae20a1ea4bf6dc4d73594c18ac9c3df3cb8
Unknown <cpost@bitbuckets.org>
2019-02-03 13:33:39 -0700

    Added AbstractPhysicsBody class to simplify body creations

--- DestinationDeepSpace/src/simulator/java/frc/robot/simulator/physics/bodies/Arm.java
+++ DestinationDeepSpace/src/simulator/java/frc/robot/simulator/physics/bodies/Arm.java
@@ -1,45 +1,57 @@
 package frc.robot.simulator.physics.bodies;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.ChainShape;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.Shape;
 import com.badlogic.gdx.physics.box2d.World;
-import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 import frc.robot.simulator.physics.MathConstants;
 
-public class Arm extends Image {
-    public Body body;
-    private World world;
-    BodyDef armBodyDef;
-    FixtureDef armFixtureDef;
-    PolygonShape armShape;
+public class Arm extends AbstractPhysicsBody {
 
     public Arm(World world, float x, float y) {
-        super(new Texture("assets/Arm.png"));
-        this.world = world;    
-        
-        BodyDef armBodyDef = new BodyDef();
-        FixtureDef armFixtureDef = new FixtureDef();
-        armBodyDef.type = BodyType.DynamicBody;
-        PolygonShape baseShape=new PolygonShape();
-        PolygonShape rightArmLowerShape=new PolygonShape();
-        PolygonShape rightArmMidShape=new PolygonShape();
-        PolygonShape rightArmUpperShape=new PolygonShape();
-        PolygonShape leftArmLowerShape=new PolygonShape();
-        PolygonShape leftArmMidShape=new PolygonShape();
-        PolygonShape leftArmUpperShape=new PolygonShape();
-//        baseShape.setAsBox(halfWidth, halfHeight);
-        /* Build a box with vertices, hw and hh are halfWidth, halfHeight
+        super(world, new Texture("assets/Arm.png"), x, y, 8.665f * 2 * MathConstants.INCHES_TO_METERS, 21 * MathConstants.INCHES_TO_METERS);
+    }
 
-        (-hw,hh)  *-------* (hw,hh)
+    private void transformInchesToMeters(Vector2[] vertices) {
+        for (int i = 0; i < vertices.length; i++) {
+            vertices[i].x = vertices[i].x * MathConstants.INCHES_TO_METERS;
+            vertices[i].y = vertices[i].y * MathConstants.INCHES_TO_METERS;
+        }
+    }
+
+    @Override
+    protected void initFixtureDef() {
+        fixtureDef.friction = 1f;
+        fixtureDef.restitution = 0;
+        fixtureDef.density = 0.1f;
+    }
+
+    /**
+     * This is a bit of a hack, but composite shapes override createShapes() and don't call
+     * createShape
+     */
+    @Override
+    protected Shape createShape() {
+        throw new IllegalArgumentException("Composite shapes don't use createShape");
+    }
+
+    @Override
+    protected List<Shape> createShapes() {
+        PolygonShape baseShape = new PolygonShape();
+        PolygonShape rightArmLowerShape = new PolygonShape();
+        PolygonShape rightArmMidShape = new PolygonShape();
+        PolygonShape rightArmUpperShape = new PolygonShape();
+        PolygonShape leftArmLowerShape = new PolygonShape();
+        PolygonShape leftArmMidShape = new PolygonShape();
+        PolygonShape leftArmUpperShape = new PolygonShape();
+        /* Build a box with vertices
+
+          (-1,14) *-------* (1,14)
                   |       |
                   |       |
                   |       |
@@ -47,7 +59,7 @@ public class Arm extends Image {
                   |       |
                   |       |
                   |       |
-        (-hw,-hh) *-------* (hw,-hh)
+           (-1,0) *-------* (1,0)
 
          */        
 
@@ -119,129 +131,23 @@ public class Arm extends Image {
         leftArmMidShape.set(leftArmMidVertices);
 
         transformInchesToMeters(leftArmUpperVertices);
-        leftArmUpperShape.set(leftArmUpperVertices);        
-
-        setSize(8.665f * 2 * MathConstants.INCHES_TO_METERS, 21 * MathConstants.INCHES_TO_METERS);
-        setOrigin(getWidth()/2, 0);
-
-        body = world.createBody(armBodyDef);
-
-        // Create a composite fixture for the arm
-        armFixtureDef.friction = 1f;
-        armFixtureDef.restitution = 0;
-        armFixtureDef.density = 0.1f;
-
-        armFixtureDef.shape = baseShape;
-        body.createFixture(armFixtureDef);
-
-        armFixtureDef.shape = rightArmLowerShape;
-        body.createFixture(armFixtureDef);
-
-        armFixtureDef.shape = rightArmMidShape;
-        body.createFixture(armFixtureDef);
-
-        armFixtureDef.shape = rightArmUpperShape;
-        body.createFixture(armFixtureDef);
-
-        armFixtureDef.shape = leftArmLowerShape;
-        body.createFixture(armFixtureDef);
-
-        armFixtureDef.shape = leftArmMidShape;
-        body.createFixture(armFixtureDef);
-
-        armFixtureDef.shape = leftArmUpperShape;
-        body.createFixture(armFixtureDef);
+        leftArmUpperShape.set(leftArmUpperVertices);   
         
-        body.setTransform(x, y, 0);
-    }
+        List<Shape> shapes = new ArrayList<>();
+        shapes.add(baseShape);
+        shapes.add(rightArmLowerShape);
+        shapes.add(rightArmMidShape);
+        shapes.add(rightArmUpperShape);
+        shapes.add(leftArmLowerShape);
+        shapes.add(leftArmMidShape);
+        shapes.add(leftArmUpperShape);
 
-    private void transformInchesToMeters(Vector2[] vertices) {
-        for (int i = 0; i < vertices.length; i++) {
-            vertices[i].x = vertices[i].x * MathConstants.INCHES_TO_METERS;
-            vertices[i].y = vertices[i].y * MathConstants.INCHES_TO_METERS;
-        }
-    }
-
-    @Override
-    public void draw(Batch batch, float parentAlpha) {
-        super.draw(batch, parentAlpha);
+        return shapes;
     }
 
     @Override
-    public void act(float delta) {
-        super.act(delta);
-        setRotation(body.getAngle()*  MathUtils.radiansToDegrees);
-        setPosition(body.getPosition().x-getWidth()/2,body.getPosition().y);
-    }
-
-    /**
-     * @return the body
-     */
-    public Body getBody() {
-        return body;
-    }
-
-    /**
-     * @param body the body to set
-     */
-    public void setBody(Body body) {
-        this.body = body;
-    }
-
-    /**
-     * @return the world
-     */
-    public World getWorld() {
-        return world;
-    }
-
-    /**
-     * @param world the world to set
-     */
-    public void setWorld(World world) {
-        this.world = world;
-    }
-
-    /**
-     * @return the armBodyDef
-     */
-    public BodyDef getarmBodyDef() {
-        return armBodyDef;
-    }
-
-    /**
-     * @param armBodyDef the armBodyDef to set
-     */
-    public void setarmBodyDef(BodyDef armBodyDef) {
-        this.armBodyDef = armBodyDef;
-    }
-
-    /**
-     * @return the armFixtureDef
-     */
-    public FixtureDef getarmFixtureDef() {
-        return armFixtureDef;
-    }
-
-    /**
-     * @param armFixtureDef the armFixtureDef to set
-     */
-    public void setarmFixtureDef(FixtureDef armFixtureDef) {
-        this.armFixtureDef = armFixtureDef;
-    }
-
-    /**
-     * @return the armShape
-     */
-    public PolygonShape getarmShape() {
-        return armShape;
-    }
-
-    /**
-     * @param armShape the armShape to set
-     */
-    public void setarmShape(PolygonShape armShape) {
-        this.armShape = armShape;
+    protected void initImageOrigin() {
+        setOrigin(getWidth() / 2, 0);
     }
     
 }
