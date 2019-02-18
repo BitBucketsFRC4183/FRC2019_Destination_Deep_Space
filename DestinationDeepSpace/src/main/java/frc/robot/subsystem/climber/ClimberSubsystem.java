73a0a191169f225fd6514c51a8389e42970bf4f2
sabzer <zerefasaba@gmail.com>
2019-02-17 17:34:35 -0700

    Climb Test

--- DestinationDeepSpace/src/main/java/frc/robot/subsystem/climber/ClimberSubsystem.java
+++ DestinationDeepSpace/src/main/java/frc/robot/subsystem/climber/ClimberSubsystem.java
@@ -12,6 +12,7 @@ import frc.robot.subsystem.BitBucketSubsystem;
 import edu.wpi.first.wpilibj.Timer;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.LimitSwitchNormal;
 import com.ctre.phoenix.motorcontrol.LimitSwitchSource;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
@@ -106,7 +107,11 @@ public class ClimberSubsystem extends BitBucketSubsystem {
 			}
 				break;
 			case HIGH_CLIMB: {
-				highClimb();
+				highClimbManual();
+				//highClimb();
+				if (climbMotor1.getSensorCollection().isFwdLimitSwitchClosed()) {
+					state=eState.IDLE;
+				}
 			}	
 				break;
 			case LOW_CLIMB: {
@@ -123,12 +128,7 @@ public class ClimberSubsystem extends BitBucketSubsystem {
 		{
 			updateDashboard();
 		}
-		
-		if (climbMotor1.getSensorCollection().isFwdLimitSwitchClosed()) {
-			climbMotor1.set(0);
-			climbMotor2.set(0);
-		}
-		
+	
 	}
 
 	@Override
@@ -178,6 +178,16 @@ public class ClimberSubsystem extends BitBucketSubsystem {
 		}
 		return (err);
 	}
+
+	private void highClimbManual()
+	{
+		climbServo.setAngle(highClimbAngle);
+		if (Timer.getFPGATimestamp() - start > 1.0) {
+			climbMotor1.set(highClimbSpeed);
+			climbMotor2.set(highClimbSpeed);
+		}
+	}
+
 
 	private int lowClimb() {
 		int err = 0;
