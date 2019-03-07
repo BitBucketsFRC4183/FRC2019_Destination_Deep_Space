1f4d351d7f81cdf2b79b74adffd1deae4ed789d9
mtkessel <the.rocketredneck@gmail.com>
2019-03-06 18:59:50 -0700

    Manually replicate Junior Changes for Guidance

--- DestinationDeepSpace/src/main/java/frc/robot/subsystem/autonomous/AutonomousSubsystem.java
+++ DestinationDeepSpace/src/main/java/frc/robot/subsystem/autonomous/AutonomousSubsystem.java
@@ -7,18 +7,9 @@
 
 package frc.robot.subsystem.autonomous;
 
-import frc.robot.operatorinterface.OI;
 import frc.robot.subsystem.BitBucketSubsystem;
-import frc.robot.utils.Controller;
-
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-
-
 
 public class AutonomousSubsystem extends BitBucketSubsystem {
-    private final OI oi = OI.instance();
 
     // Singleton method; use AutonomousSubsystem.instance() to get the AutonomousSubsystem instance.
 	public static AutonomousSubsystem instance() {
@@ -38,12 +29,6 @@ public class AutonomousSubsystem extends BitBucketSubsystem {
 	protected void initDefaultCommand() {
     }
     
-
-
-    private boolean inAutoAssistRegion = false;
-    private double parallax = 0;
-    private double offAxis = 0;
-
     @Override
 	public void periodic() {
     }
@@ -62,41 +47,6 @@ public class AutonomousSubsystem extends BitBucketSubsystem {
 	public void diagnosticsCheck() {		
     }
     
-
-
-    private NetworkTableInstance networkTable = NetworkTableInstance.getDefault();
-	private NetworkTable bvTable = networkTable.getTable("BucketVision");
-	private NetworkTableEntry bvStateEntry = bvTable.getEntry("BucketVisionState");
-    private NetworkTableEntry bvCameraNumber = bvTable.getEntry("CameraNum");
-    
-    public CameraFeedback getClosestObjectData() {
-        // TODO: implement - get the CameraFeedback for the closest target
-
-        int numTargets = (int) bvTable.getEntry("NumTargets").getValue().getDouble();
-        if (numTargets == 0) {
-            return null; // null if no target found (do we want this behavior?)
-        }
-
-        double[] distance = bvTable.getEntry("distance").getValue().getDoubleArray();
-        double[] pos_x = bvTable.getEntry("pos_x").getValue().getDoubleArray();
-        double[] parallax = bvTable.getEntry("parallax").getValue().getDoubleArray();
-
-        int min_index = 0;
-        double min_distance = distance[0];
-        for (int i = 0; i < numTargets; i++) {
-            if (distance[i] < min_distance) {
-                min_index = i;
-                min_distance = distance[i];
-            }
-        }
-
-        double offAxis = 2 * pos_x[min_index] - 1; // normalize to [-1, 1] from [0, 1]
-
-        return null;
-    }
-
-
-
     @Override
 	public void initialize() {
 		initializeBaseDashboard();
