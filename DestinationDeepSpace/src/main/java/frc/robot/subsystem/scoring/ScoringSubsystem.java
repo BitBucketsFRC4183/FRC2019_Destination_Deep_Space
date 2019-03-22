3c45ab5e3a72d39e8a40b73d6aae332ab1c94385
mtkessel <the.rocketredneck@gmail.com>
2019-03-21 20:46:27 -0700

    Airshow changes

--- DestinationDeepSpace/src/main/java/frc/robot/subsystem/scoring/ScoringSubsystem.java
+++ DestinationDeepSpace/src/main/java/frc/robot/subsystem/scoring/ScoringSubsystem.java
@@ -132,6 +132,9 @@ public class ScoringSubsystem extends BitBucketSubsystem {
 		if (angle <= -180) {
 			angle += 360;
 		}
+		else if (angle >= 180) {
+			angle -= 360;
+		}
 		setAngle_deg(angle);
 	}
 
